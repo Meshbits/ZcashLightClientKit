f925b627a4a49ceab631856daa8703e8fc1393eb
Satinder Grewal <grewal@lavabit.com>
2021-05-21 07:14:48 +1200

    sample app updated to ARRR tests in UI

--- Example/ZcashLightClientSample/ZcashLightClientSample/Send/SendViewController.swift
+++ Example/ZcashLightClientSample/ZcashLightClientSample/Send/SendViewController.swift
@@ -79,7 +79,7 @@ class SendViewController: UIViewController {
     
     
     func format(balance: Int64 = 0) -> String {
-        "Zec \(balance.asHumanReadableZecBalance())"
+        "ARRR \(balance.asHumanReadableZecBalance())"
     }
     
     func toggleSendButton() {
